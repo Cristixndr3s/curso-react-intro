0b694bb41e9306f435741bc37749d4efb38e1fd7
cristixndr3s <cristiandres17@gmail.com>
2023-09-30 15:43:58 -0500

    intro

--- src/App.js
+++ src/App.js
@@ -23,6 +23,14 @@ function App() {
     todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+   );
+
   return (
     <>
       <TodoCounter 
@@ -34,7 +42,7 @@ function App() {
         setSearchValue={setSearchValue}
       />
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem key={todo.text} 
           text={todo.text}
           completed={todo.completed}
